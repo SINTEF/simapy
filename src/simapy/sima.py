7597f9e808fcc9fa47512c48d49ac504ae0abd75
Lasse Bjermeland <lasse.bjermeland@sintef.no>
2023-04-26 09:22:18 +0200

    Use asyncio to run SRE

--- src/simapy/sima.py
+++ src/simapy/sima.py
@@ -1,54 +1,69 @@
-"""SIMA executable wrapper class"""
-import subprocess
+"""Run a command using SIMA runtime engine (sre executable)"""
+import asyncio
 import os
-from typing import List, Sequence
 from pathlib import Path
+from typing import Sequence
+
+
+async def run_command(args, error_file, out_file):
+    """"Run a command asynchronously and print output to standard out. 
+    Standard err is piped to stderr.txt in the working directory"""
+    process = await asyncio.create_subprocess_exec(
+        *args, stdout=asyncio.subprocess.PIPE, stderr=error_file
+    )
+
+    while True:
+        stdout, _ = await process.communicate()
+
+        if stdout:
+            for line in stdout.splitlines():
+                sline = line.decode("utf-8")
+                if sline.startswith("@STATUS"):
+                    print("STATUS found:" + sline, end="\n")
+                else:
+                    print(sline, end="\n")
+                    print(sline, end="\n", file=out_file)
+
+        if process.returncode is not None:
+            return process.returncode
 
 
 class SIMA:
     """"Run a command using SIMA runtime engine (sre executable)"""
 
-    def __init__(self, exe=None) -> None:
-        self.exe = self.__get_executable(exe)
-        self.proc = None
-
-    def __get_executable(self, exe):
+    def __init__(self, exe=None, fail_on_error=True):
+        self.fail_on_error = fail_on_error
         if exe:
-            return exe
-        exe =  os.getenv('SIMA_EXE')
-        if not exe:
-            raise Exception("No executable given, and SIMA_EXE environment variable is not set")
-        return exe
-
-    def run(self, workspace: str, commands: Sequence[str], env=None) -> Sequence[str]:
-        """Run the sima executable and return output from standard out and standard err"""
-        if not Path(self.exe).exists():
-            raise Exception(f"Executable does not exist: {self.exe}")
-        args = [self.exe]
-        args.append("-consoleLog")
-        args.append("--progress")
-        args.append("-data")
-        args.append(workspace)
-        args = args + commands
+            self.exe = exe
+        else:
+            self.exe =  os.getenv('SRE_EXE',os.getenv('SIMA_EXE'))
+            if not self.exe:
+                raise ValueError("No executable given, and SRE_EXE environment variable is not set")
 
 
-        self.proc = subprocess.Popen(args,
-                            stdin=subprocess.PIPE,
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.STDOUT,env=env)
-        return self.__read_lines()
+    def run(self, working_dir: Path, command_args: Sequence[str]):
+        """Run the sima executable and print output to standard out. 
+           Standard err is piped to stderr.txt in the working directory"""
+        arguments = [self.exe]
+        arguments.extend(["-configuration", "/var/opt/sima/config"])
+        arguments.append("-consoleLog")
+        arguments.append("--progress")
+        arguments.extend(["-data", working_dir])
+        arguments.extend(command_args)
+        working_dir.mkdir(parents=True, exist_ok=True)
+        err_file = working_dir / "stderr.txt"
+        out_file = working_dir / "stdout.txt"
+        with open(err_file, "w", encoding="utf8") as ef, open(
+            out_file, "w", encoding="utf8"
+        ) as of:
+            exit_code = asyncio.run(run_command(arguments, ef, of))
+            if exit_code != 0 and self.fail_on_error:
+                raise RuntimeError(f"SIMA exited with error code {exit_code}")
 
-
-    def __read_lines(self) -> List[str]:
-        proc = self.proc
-        lines = []
-        while True:
-            line = proc.stdout.readline()
-            if not line:
-                break
-            lines.append(line.decode(encoding="utf8").rstrip())
-
-        return lines
+    def run_command(self, working_dir: Path, command_file: Path):
+        """Run commands from a file"""
+        args = ["--commands",f"file={command_file}"]
+        self.run(working_dir,args)
 
 def main():
     """Run a single command and read back the result"""
@@ -57,8 +72,8 @@ def main():
 
     sima = SIMA()
     commands = ["--help","all"]
-    res = sima.run(workspace,commands)
-    print(res)
+    sima.run(workspace,commands)
+
 
 if __name__ == '__main__':
     main()
