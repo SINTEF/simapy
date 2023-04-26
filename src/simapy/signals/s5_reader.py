a39b30a57849a26a7aec03252540ef20c0b6ab86
Lasse Bjermeland <lasse.bjermeland@sintef.no>
2023-04-26 10:09:28 +0200

    Create test signals from different formats

---
+++ src/simapy/signals/s5_reader.py
@@ -0,0 +1,245 @@
+""" Export entites as h5"""
+from collections import OrderedDict
+from importlib import import_module
+from typing import Callable, Dict, Sequence
+from marmo import containers
+
+import h5py as h5
+import json
+import numpy as np
+
+from dmt.attribute import Attribute
+from dmt.blueprint_attribute import BlueprintAttribute
+from dmt.entity import Entity
+from dmt.enum_attribute import EnumAttribute
+
+
+class S5Reader:
+    """Read entities from S5 file"""
+
+    class Reference:
+        """Holds a reference until it can be resolved"""
+
+        def __init__(self, entity, prop: Attribute, uid: str):
+            self.entity = entity
+            self.prop = prop
+            self.uid = uid
+
+    def __init__(
+        self, external_refs: Dict[str, Entity] = None, root_package: str = None
+    ):
+        self.root_package: str = root_package
+        self.entities = dict()
+        self.unresolved = list()
+        self.external_refs = dict()
+        if external_refs:
+            self.external_refs = external_refs
+        self.datasource = None
+
+    def read(self, filename) -> Sequence[Entity]:
+        """Read entities from s5 file"""
+        entities = []
+        with h5.File(filename, "r") as root:
+            vds: h5.Dataset = root["version"]
+            self.__h5_file = root
+            version = vds.asstr()[0]
+            if version != "2.0.0":
+                raise ValueError(f"Unsupported version {version}")
+            signals: h5.Dataset = root["signals"]
+            json_data = signals.asstr()[0]
+            entities_dict = json.loads(json_data)
+            for name, dicts in entities_dict.items():
+                entities.extend(self.__read_group(name, dicts))
+            # for group in root.values():
+            #     entities.append(self.__read_group(group))
+
+        self.__resolve_all()
+        return entities
+
+    def __resolve_all(self):
+        for ref in self.unresolved:
+            if not self.__resolve(ref):
+                raise Exception(f"Unresolved reference: {ref}")
+
+    def __read_group(self, name, e_dicts: Sequence[Dict]) -> Entity:
+        """Read entities from Dict"""
+        if name == "c":
+            entities = []
+            for e_dict in e_dicts:
+                entities.append(self.__read_container(e_dict))
+            return entities
+
+        entity_type: str = e_dict.attrs["type"]
+        constructor = self._resolve_type(entity_type)
+        if not constructor:
+            raise Exception(f"Unkown entity type {entity_type}")
+        entity_instance: Entity = constructor()
+        blueprint = entity_instance.blueprint
+        for name, node in e_dict.items():
+            if name == "_id":
+                uid = node[()].decode()
+                self.entities[uid] = entity_instance
+                continue
+            attribute = blueprint.get_attribute(name)
+            if not attribute:
+                # FIXME
+                continue
+            if isinstance(attribute, BlueprintAttribute):
+                self.__set_blueprint_value(entity_instance, attribute, node)
+            elif attribute.is_enum:
+                value = node[()].decode()
+                self.__set_enum_value(entity_instance, attribute, value)
+            else:
+                value = node[()]
+                if attribute.is_string():
+                    if attribute.has_dimensions():
+                        setattr(entity_instance, name, value.astype(str))
+                    else:
+                        setattr(entity_instance, name, value.decode())
+                else:
+                    setattr(entity_instance, name, value)
+
+        return entity_instance
+
+    def __read_container(self, e_dict: Dict) -> containers.Container:
+        c = containers.Container(name=e_dict["name"])
+        c_dicts = e_dict.get("c")
+        if c_dicts:
+            for c_dict in c_dicts:
+                c.containers.append(self.__read_container(c_dict))
+        s_dicts = e_dict.get("s")
+        if s_dicts:
+            for s_dict in s_dicts:
+                s = self.__read_signal(s_dict)
+                if s:
+                    c.signals.append(s)
+
+        return c
+
+    def __read_signal(self, s_dict: Dict) -> containers.Signal:
+        sname = s_dict["name"]
+        props = s_dict.get("p",{})
+        for name, dvalue in s_dict.items():
+            if name == "n":
+                return containers.DimensionalScalar(
+                    name=sname, value=dvalue["v"], unit=dvalue.get("yunit")
+                )
+            if name == "ns":
+                signal = containers.EquallySpacedSignal(name=sname)
+                signal.xdelta = dvalue.get("dlt", signal.xdelta)
+                signal.xunit = dvalue.get("xunit", signal.xunit)
+                signal.unit = dvalue.get("yunit", signal.unit)
+                signal.xstart = dvalue.get("st", signal.xstart)
+                path = dvalue.get("path")
+                if path:
+                    # Found within the h5-file..
+                    signal.value = self.__h5_file[path]
+                else:
+                    signal.value = dvalue["v"]
+                return signal
+            if name == "xy":
+                signal = containers.NonEquallySpacedSignal(name=sname)
+                for name, value in props.items():
+                    if name == "ylabel":
+                        name = "label"
+                    setattr(signal, name, value)
+
+                signal.xunit = dvalue.get("xunit", signal.xunit)
+                signal.unit = dvalue.get("yunit", signal.unit)
+                path = dvalue.get("path")
+                if path:
+                    # Found within the h5-file..
+                    ds = self.__h5_file[path]
+                    sets = np.split(ds, 2)
+                    signal.xvalue = sets[0]
+                    signal.value = sets[1]
+                return signal
+            elif name == "s":
+                return containers.SimpleString(name=sname, value=dvalue["v"])
+            elif name == "i":
+                return containers.IntegerScalar(name=sname, value=dvalue["v"])
+            elif name == "b":
+                return containers.SimpleBoolean(name=sname, value=dvalue["v"])
+            elif name == "ss":
+                path = dvalue.get("path")
+                if path:
+                    # Found within the h5-file..
+                    ds = self.__h5_file[path].asstr()
+                    return containers.StringArray(name=sname, value=ds)
+                return containers.StringArray(name=sname, value=dvalue["v"])
+
+        raise ValueError("Unexpected signal type:" + json.dumps(s_dict))
+
+    def _resolve_type(self, atype: str) -> Callable:
+        pkg: any = None
+        parts = atype.split("/")
+        if self.datasource:
+            parts.remove(self.datasource)
+        if parts[0] == "":
+            del parts[0]
+        ename = parts.pop()
+        package_path = ".".join(parts)
+        if self.root_package:
+            package_path = self.root_package + "." + package_path
+        try:
+            pkg = import_module(package_path)
+        except ModuleNotFoundError as error:
+            raise Exception(f"Unable to load package {package_path}") from error
+
+        constructor = pkg.__dict__.get(ename)
+        return constructor
+
+    def __set_blueprint_value(
+        self, entity_instance: Entity, attribute: BlueprintAttribute, value
+    ):
+        if attribute.contained:
+            self.__set_value(entity_instance, attribute, value)
+        else:
+            self.__set_reference(entity_instance, attribute, value)
+
+    def __set_reference(
+        self, entity_instance: Entity, prop: Attribute, group: h5.Group
+    ):
+        uid = group.get("_id")[()].decode()
+        ref = self.Reference(entity_instance, prop, uid)
+        if not self.__resolve(ref):
+            self.unresolved.append(ref)
+        return
+
+    def __set_value(
+        self, entity_instance: Entity, attribute: BlueprintAttribute, value
+    ):
+        if attribute.has_dimensions():
+            root: h5.Group = value
+            od = OrderedDict()
+            children = []
+            for name, v in root.items():
+                od[name] = self.__read_group(v)
+
+            order = root.attrs.get("order")
+            if order is not None:
+                sorder = order[()]
+                for name in sorder:
+                    children.append(od[name])
+            else:
+                children = list(od.values())
+            setattr(entity_instance, attribute.name, children)
+        else:
+            setattr(entity_instance, attribute.name, self.__read_group(value))
+
+    def __resolve(self, ref: Reference):
+        value = self.entities.get(ref.uid, self.external_refs.get(ref.uid, None))
+        if value:
+            setattr(ref.entity, ref.prop.name, value)
+            return True
+
+        return False
+
+    def __set_enum_value(self, entity: Entity, attribute: EnumAttribute, value: str):
+        """Convert from string to Enum"""
+
+        constructor = self._resolve_type(attribute.type)
+        if not constructor:
+            raise Exception(f"Unkown Enum type {attribute.type}")
+        evalue = constructor[value]
+        setattr(entity, attribute.name, evalue)
--- src/simapy/sima_reader.py
+++ src/simapy/sima_reader.py
@@ -1,16 +1,26 @@
 """
-Reader that handles SIMA specific JSON
+Reader that handles SIMA specific .json or .s5 files.
 """
 
+import os
 from typing import Sequence
 from dmt.entity import Entity
 from dmt.dmt_reader import DMTReader
 from sima.modelcontent import ModelContent
+from simapy.signals.s5_reader import S5Reader
 
 class SIMAReader():
     """ Creates entities from Dicts or file """
 
     def read(self, filename) -> Sequence[Entity]:
         """ Read entity from file """
-        modelcontent: ModelContent =  DMTReader().read(filename)
-        return modelcontent.contents
+        if self.__is_s5(filename):
+            return  S5Reader().read(filename)
+        else:
+            modelcontent: ModelContent =  DMTReader().read(filename)
+            return modelcontent.contents
+
+    def __is_s5(self,filename):
+        _, extension = os.path.splitext(filename)
+        extension = extension.lower()
+        return extension=='.s5'
